f728e06773694b21409a4f3f277b83897076de82
ShenJing <shenjingnerve@gmail.com>
2021-01-03 18:01:37 +0800

    [FEAT] UVa 10003 (AC)
    ###### tags: `Dynamic Programming`
    
    - Top Down DP

--- UVa/UVa10003_Cutting Sticks.cpp
+++ UVa/UVa10003_Cutting Sticks.cpp
@@ -35,20 +35,17 @@ vector<int> places;
 int solve(int start, int end)
 {
     if (end - start == 1)
-    {
-        dp[start][end] = places[end] - places[start];
+        dp[start][end] = 0;
+
+    if (dp[start][end] != -1)
         return dp[start][end];
-    }
     
     int min_cost{numeric_limits<int>::max()};
-    for (int cut_place = 1; cut_place < num_of_cuts; ++cut_place)
+    for (int cut_place = start + 1; cut_place < end; ++cut_place)
     {
         int cost_of_this_cut = (places[end] - places[start]) + solve(start, cut_place) + solve(cut_place, end);
         if (cost_of_this_cut < min_cost)
-        {
             min_cost = cost_of_this_cut;
-            // (places[end] - places[start]) + dp[start][cut_place] + dp[cut_place][end];
-        }
     }
     dp[start][end] = min_cost;
     return min_cost;
@@ -57,7 +54,7 @@ int solve(int start, int end)
 int main(void)
 {
     int stick_len;
-    while (cin >> stick_len)
+    while (cin >> stick_len && stick_len != 0)
     {
         cin >> num_of_cuts;
         places.assign({0});
@@ -70,8 +67,8 @@ int main(void)
         }
         places.emplace_back(stick_len);
 
-        dp.assign(num_of_cuts, vector<int>(num_of_cuts + 1, 0));
-        solve(0, num_of_cuts + 1);
+        dp.assign(num_of_cuts + 1, vector<int>(num_of_cuts + 2, -1));
+        cout << "The minimum cutting is " << solve(0, num_of_cuts + 1) << ".\n";
     }
 
     return 0;
