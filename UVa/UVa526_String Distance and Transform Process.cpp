c53609848b13ae665d2bf8b24f7bcfcb336017cc
ShenJing <shenjingnerve@gmail.com>
2021-02-22 23:58:36 +0800

    [FEAT] UVa 526: draft

---
+++ UVa/UVa526_String Distance and Transform Process.cpp
@@ -0,0 +1,45 @@
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <fstream>
+#include <functional>
+#include <initializer_list>
+#include <iostream>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <tuple>
+#include <type_traits>
+#include <utility>
+#include <regex>
+#include <set>
+#include <stack>
+#include <string>
+#include <type_traits>
+#include <vector>
+
+using namespace std;
+
+static auto io = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    return nullptr;
+}();
+
+int main(void)
+{
+    // char query[80], target[80];
+    string query, target;
+    while (cin >> query)
+    {
+        cin >> target;
+        query = string(" ") + query;
+        target = string(" ") + target;
+    }
+
+    return 0;
+}
