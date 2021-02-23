9c882d913d35691fd463fff5c34f9a526ca2e70d
ShenJing <shenjingnerve@gmail.com>
2021-02-23 10:05:02 +0800

    [FEAT] UVa 526: Rough structure
    - Bug: access out of range value

--- UVa/UVa526_String Distance and Transform Process.cpp
+++ UVa/UVa526_String Distance and Transform Process.cpp
@@ -34,11 +34,30 @@ int main(void)
 {
     // char query[80], target[80];
     string query, target;
+    constexpr int k_max_str_size{80};
+    array<array<int, k_max_str_size>, k_max_str_size> edit_distance;
     while (cin >> query)
     {
         cin >> target;
+
+        auto query_len = query.length();
+        auto target_len = target.length();
         query = string(" ") + query;
         target = string(" ") + target;
+        for (size_t q = 0; q <= query_len; ++q)
+        {
+            for (size_t t = 0; t <= target_len; ++t)
+            {
+                if (!q && !t)
+                    continue;
+                edit_distance[q][t] = min(edit_distance[q - 1][t] + 1, edit_distance[q][t - 1] + 1);
+                if (query[q] == target[t])
+                    edit_distance[q][t] = min(edit_distance[q][t], edit_distance[q - 1][t - 1]);
+                else
+                    edit_distance[q][t] = min(edit_distance[q][t], edit_distance[q - 1][t - 1] + 1);
+            }
+        }
+        cout << edit_distance[query_len][target_len] << "\n";
     }
 
     return 0;
