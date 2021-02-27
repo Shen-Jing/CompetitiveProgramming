f6d9e01775111ca4adf8fbaf497202a94b7db08f
ShenJing <shenjingnerve@gmail.com>
2021-02-27 10:54:04 +0800

    [FEAT] utils.hpp (split function): rough version

--- utils/utils.hpp
+++ utils/utils.hpp
@@ -1,6 +1,23 @@
+#include <vector>
+#include <string>
+#include <string_view>
+#include <type_traits>
 #include <boost/algorithm/string.hpp>
 
-void split(vector<string> &result, string &target, string_view &delim)
+// template <typename T>
+// void split(std::vector<T> &tokens, std::string &target, std::string &delim)
+void split(std::vector<std::string> &tokens, std::string &target, std::string &delim)
 {
-    boost::split(result, target, boost::is_any_of(delim));
+    std::vector<std::string> split_result;
+    boost::split(split_result, target, boost::is_any_of(delim));
+
+    // if (std::is_integral<T>::value)
+    // {
+    //     for (auto &&token : split_result)
+    //         tokens.emplace_back(stoi(token));
+    // }
+    // else
+    // {
+        tokens = std::move(split_result);
+//     }
 }
