8749e18d3207a02a528daa8cc265f477a2d7e75b
ShenJing <shenjingnerve@gmail.com>
2021-03-07 17:02:19 +0800

    [FEAT] utils: LeetCode definition of basic data structure
    - ListNode
    - TreeNode

--- utils/utils.hpp
+++ utils/utils.hpp
@@ -4,6 +4,23 @@
 #include <type_traits>
 #include <boost/algorithm/string.hpp>
 
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
 // template <typename T>
 // void split(std::vector<T> &tokens, std::string &target, std::string &delim)
 void split(std::vector<std::string> &tokens, std::string &target, std::string &delim)
