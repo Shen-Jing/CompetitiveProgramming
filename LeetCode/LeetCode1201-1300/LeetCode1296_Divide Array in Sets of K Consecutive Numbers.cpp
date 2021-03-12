5630847b8e03be2302cb3875ce6dff0e5c703266
ShenJing <shenjingnerve@gmail.com>
2021-03-12 22:03:34 +0800

    [FEAT] LC 1296 (WA): Imitating No.128 but failed.

---
+++ LeetCode/LeetCode1201-1300/LeetCode1296_Divide Array in Sets of K Consecutive Numbers.cpp
@@ -0,0 +1,83 @@
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <fstream>
+#include <functional>
+#include <initializer_list>
+#include <iostream>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <tuple>
+#include <type_traits>
+#include <utility>
+#include <regex>
+#include <set>
+#include <stack>
+#include <sstream>
+#include <string>
+#include <type_traits>
+#include <vector>
+
+using namespace std;
+
+static auto io = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    return nullptr;
+}();
+
+class Solution
+{
+ public:
+    bool isPossibleDivide(vector<int>& nums, int k) 
+    {
+        unordered_map<int, int> occurencies;
+        for (int num : nums)
+        {
+            if (!occurencies.count(num))
+                occurencies[num] = 0;
+            ++occurencies[num];
+        }
+        
+        for (const auto &num : nums)
+        {
+            if (!occurencies.count(num - 1))
+            {
+                int cur_streak{1};
+                for (auto cur_num = num + 1; occurencies.count(cur_num) >= 1; ++cur_num)
+                {
+                    ++cur_streak;
+                    if (cur_streak == k)
+                        break;
+                }
+
+                if (cur_streak == k)
+                {
+                    /* Removing current set of k consecutive numbers */
+                    for (int value{num}; value < num + k; ++value)
+                    {
+                        --occurencies[value];
+                        if (!occurencies[value])
+                            occurencies.erase(value);
+                    }
+                }
+                else
+                    return false;
+            }
+        }
+        return true;
+    }
+};
+
+int main(void)
+{
+    Solution sol;
+    vector<int> nums{1,2,3,3,4,4,5,6};
+    sol.isPossibleDivide(nums, 4);
+    return 0;
+}
