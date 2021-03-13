fd03a28ddabf0760ecf1f933c765767a4b965faf
ShenJing <shenjingnerve@gmail.com>
2021-03-13 23:44:59 +0800

    [FEAT] LC 283 (AC): Not in-place.
    What idea is behind my first commit in 2019? I don't know...

--- LeetCode/LeetCode201-300/LeetCode283_Move Zeroes.cpp
+++ LeetCode/LeetCode201-300/LeetCode283_Move Zeroes.cpp
@@ -1,22 +1,59 @@
-static auto io = []()
-{
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(nullptr);
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <fstream>
+#include <functional>
+#include <initializer_list>
+#include <iostream>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <tuple>
+#include <type_traits>
+#include <utility>
+#include <regex>
+#include <set>
+#include <stack>
+#include <sstream>
+#include <string>
+#include <type_traits>
+#include <vector>
+
+using namespace std;
+
+static auto io = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
     return nullptr;
 }();
-class Solution {
-public:
-    void moveZeroes(vector<int>& nums) {
+
+class Solution
+{
+ public:
+    void moveZeroes(vector<int>& nums)
+    {
+        vector<int> ans;
         int cnt = 0;
         for (int i = 0; i < nums.size(); ++i)
         {
             if (!nums[i])
                 ++cnt;
             else
-                nums[i - cnt] = nums[i];
+                ans.emplace_back(nums[i]);
         }
-        //std::transform(nums.begin() + cnt + 1, nums.end(), nums.begin() + cnt + 1, 
-                       //[](int ele) { return 0; });
+        for (int i = 0; i < cnt; ++i)
+            ans.emplace_back(0);
+        nums = move(ans);
         return;
     }
 };
+
+int main(void)
+{
+
+    return 0;
+}
