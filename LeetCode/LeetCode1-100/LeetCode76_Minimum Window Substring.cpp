500290e96595702d703eedf155d7e3c0d110ab80
ShenJing <shenjingnerve@gmail.com>
2021-03-25 01:44:39 +0800

    [FEAT] LC 76 (AC): (No optimized) Sliding Window

--- LeetCode/LeetCode1-100/LeetCode76_Minimum Window Substring.cpp
+++ LeetCode/LeetCode1-100/LeetCode76_Minimum Window Substring.cpp
@@ -30,38 +30,69 @@ static auto io = [](){
     return nullptr;
 }();
 
+enum Ans
+{
+    k_min_len = 0,
+    k_left,
+    k_right,
+};
+
 class Solution
 {
  public:
     string minWindow(string s, string t)
     {
-        set<char> lack_chars, target_chars;
+        unordered_map<char, int> cur_freq, target_freq;
         for (const auto &ch : t)
-        {
-            lack_chars.emplace(ch);
-            target_chars.emplace(ch);
-        }
-        size_t left = 0, right = 0;
+            ++target_freq[ch];
+
+        auto required = target_freq.size();
+        decltype(required) formed = 0;
         auto sz = s.size();
-        for ( ; left < sz && right < sz; )
+        
+        array<int, 3> ans{static_cast<int>(sz) + 1, 0, -1};
+        for (size_t left = 0, right = 0; left < sz && right < sz; ++right)
         {
-            if (lack_chars.count(s[right]) && lack_chars.size() == 1)
+            auto cur_char = s[right];
+            ++cur_freq[cur_char];
+            if (target_freq.count(cur_char) && cur_freq[cur_char] == target_freq[cur_char])
             {
-                lack_chars.erase(s[right]);
-                lack_chars.emplace(s[left]);
-                ++left;
-            }
-            else
-            {
-                ++right;
+                ++formed;
+                while (formed == required && left < sz)
+                {
+                    if (ans[Ans::k_min_len] > right - left + 1)
+                    {
+                        // cout << ans[Ans::k_min_len] << " " <<  ans[Ans::k_left]  << ", "<< ans[Ans::k_right] << "\n";
+                        ans[Ans::k_min_len] = right - left + 1;
+                        ans[Ans::k_left] = left;
+                        ans[Ans::k_right] = right;
+                    }
+                    auto removed = s[left];
+                    --cur_freq[removed];
+                    ++left;
+
+                    if (target_freq.count(removed) && cur_freq[removed] < target_freq[removed])
+                    {
+                        --formed;
+                        break;
+                    }
+                }
             }
         }
-        return s.substr(left, right - left + 1);
+        if (ans[Ans::k_right] == -1)
+            return "";
+        /* Cannot use `ans` as length of answer (with **no check**).
+           Otherwise it will get WA when no answer (i.g., s = "a", t = "b") */
+        return s.substr(ans[Ans::k_left], ans[Ans::k_min_len]);
     }
 };
 
 int main(void)
 {
-
+    Solution sol;
+    cout << sol.minWindow(string("ADOBECODEBANC"), string("ABC")) << "\n";
+    cout << sol.minWindow(string("cabwefgewcwaefgcf"), string("cae")) << "\n";
+    cout << sol.minWindow(string("a"), string("a")) << "\n";
+    cout << sol.minWindow(string("a"), string("b")) << "\n";
     return 0;
 }
