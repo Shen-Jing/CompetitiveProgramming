04e36a60e30607b32a6581a6279600092db1f1c3
ShenJing <shenjingnerve@gmail.com>
2021-04-05 19:38:47 +0800

    [FEAT] (2019/12/14) UVa 12657

---
+++ UVa/UVa12657_Boxes in a Line.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+
+int main(void)
+{
+    int n, m;
+    while (std::cin >> n >> m)
+    {
+        for (int i = 1; i <= m; ++i)
+        {
+            int cmd, x, y;
+            std::cin >> cmd;
+            if (cmd == 1)
+            {
+
+            }
+            else if (cmd == 2)
+            {
+
+            }
+            else if (cmd == 3)
+            {
+
+            }
+            else if (cmd == 4)
+            {
+
+            }
+        }
+    }
+}
