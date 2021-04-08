f230574d959661f5a2b2de1b5a5332d5021296d9
ShenJing <shenjingnerve@gmail.com>
2021-04-08 23:02:45 +0800

    [FEAT] LC 226 (AC): Recursive

---
+++ LeetCode/LeetCode201-300/LeetCode226_Invert Binary Tree.cpp
@@ -0,0 +1,54 @@
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <fstream>
+#include <functional>
+#include <initializer_list>
+#include <iostream>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <tuple>
+#include <type_traits>
+#include <utility>
+#include <regex>
+#include <set>
+#include <stack>
+#include <sstream>
+#include <string>
+#include <type_traits>
+#include <vector>
+#include "utils.hpp"
+
+using namespace std;
+
+static auto io = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    return nullptr;
+}();
+
+class Solution
+{
+ public:
+    TreeNode* invertTree(TreeNode* root)
+    {
+        if (!root)
+            return nullptr;
+
+        swap(root->left, root->right);
+        root->left = (root->left) ? invertTree(root->left) : nullptr;
+        root->right = (root->right) ? invertTree(root->right) : nullptr;
+        return root;
+    }
+};
+
+int main(void)
+{
+
+    return 0;
+}
