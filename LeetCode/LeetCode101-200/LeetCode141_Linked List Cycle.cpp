3517921403ea65d660f6db3110fc2ad8027e4c8b
ShenJing <shenjingnerve@gmail.com>
2021-04-26 12:44:07 +0800

    [FEAT] LC 141: Rewrite

--- LeetCode/LeetCode101-200/LeetCode141_Linked List Cycle.cpp
+++ LeetCode/LeetCode101-200/LeetCode141_Linked List Cycle.cpp
@@ -1,47 +1,78 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <fstream>
+#include <functional>
+#include <initializer_list>
+#include <iostream>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <tuple>
+#include <type_traits>
+#include <utility>
+#include <regex>
+#include <set>
+#include <span>
+#include <stack>
+#include <sstream>
+#include <string>
+#include <type_traits>
+#include <vector>
+#include "utils.hpp"
+
+using namespace std;
+
 static auto io = [](){
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
     return nullptr;
-};
-class Solution {
-public:
-    bool hasCycle(ListNode *head) {
-        ListNode *slow{head};
-        while (slow)
+}();
+
+class Solution
+{
+ public:
+    bool hasCycle(ListNode *head)
+    {
+        return check_cycle_by_hash_table(head);
+        return check_cycle_by_two_pointers(head);
+    }
+ 
+ private:
+    bool check_cycle_by_two_pointers(ListNode *head)
+    {
+        ListNode *slow = head, *fast = head;
+        while (!fast && !fast->next)
         {
-            /* faster pointer */
-            if (!head || !head->next)
-                return false;
-            head = head->next->next;
+            fast = fast->next->next;
             slow = slow->next;
-            if (slow == head)
+
+            if (slow == fast)
                 return true;
         }
         return false;
     }
-    /*
-    bool hasCycle(ListNode *head) {
-        std::map<ListNode *, int> nd2pos;
-        int pos = 0;
+
+    bool check_cycle_by_hash_table(ListNode *head)
+    {
+        unordered_set<ListNode *> nodes;
         while (head)
         {
-            nd2pos[head] = pos++;
-            head = head->next;
-            if (nd2pos.count(head))
-            {
-                std::cout << "Position: " << nd2pos[head] << " (val: " << head->val << ")" << std::endl;
+            if (nodes.count(head))
                 return true;
-            }
+            nodes.emplace(head);
+            head = head->next;
         }
         return false;
     }
-    */
 };
+
+int main(void)
+{
+
+    return 0;
+}
