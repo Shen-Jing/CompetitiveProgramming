87d605335798d66195f2da10b3365d580a8f90f3
ShenJing <shenjingnerve@gmail.com>
2021-04-27 18:23:10 +0800

    [FEAT] LC 206: (Rewriting) Iterative

--- LeetCode/LeetCode201-300/LeetCode206_Reverse Linked List.cpp
+++ LeetCode/LeetCode201-300/LeetCode206_Reverse Linked List.cpp
@@ -1,27 +1,69 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
-class Solution {
-public:
-    ListNode* reverseList(ListNode* head) {
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <fstream>
+#include <functional>
+#include <initializer_list>
+#include <iostream>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <tuple>
+#include <type_traits>
+#include <utility>
+#include <regex>
+#include <set>
+#include <span>
+#include <stack>
+#include <sstream>
+#include <string>
+#include <type_traits>
+#include <vector>
+#include "utils.hpp"
+
+using namespace std;
+
+static auto io = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    return nullptr;
+}();
+
+class Solution
+{
+ public:
+    ListNode* reverseList(ListNode* head)
+    {
+        return reverse_list_iterative(head);
+    }
+
+ private:
+    ListNode* reverse_list_iterative(ListNode *head)
+    {
         if (!head)
-            return head;
-        ListNode * pre = nullptr;
-        ListNode * cur = head;
-        ListNode * nxt = head->next;
-        while (cur && cur->next)
+            return nullptr;
+        ListNode *prev = nullptr, *curr = head;
+        while (curr)
         {
-            cur->next = pre;
-            pre = cur;
-            cur = nxt;
-            nxt = cur->next;
+            ListNode *tmp_next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = tmp_next;
         }
-        cur->next = pre;
-        return cur;
+        return prev;
+    }
+
+    ListNode* reverse_list_recursive(ListNode *head)
+    {
     }
 };
+
+int main(void)
+{
+
+    return 0;
+}
