f8b4c620cdd50763695db576d30ac0b879cb5d87
ShenJing <shenjingnerve@gmail.com>
2021-04-27 21:16:19 +0800

    [FEAT] LC 98: Incorrect draft (inorder)

---
+++ LeetCode/LeetCode1-100/LeetCode98_Validate Binary Search Tree.cpp
@@ -0,0 +1,63 @@
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <fstream>
+#include <functional>
+#include <initializer_list>
+#include <iostream>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <tuple>
+#include <type_traits>
+#include <utility>
+#include <regex>
+#include <set>
+#include <stack>
+#include <sstream>
+#include <string>
+#include <type_traits>
+#include <vector>
+#include "utils.hpp"
+
+using namespace std;
+
+static auto io = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    return nullptr;
+}();
+
+class Solution
+{
+ public:
+    bool isValidBST(TreeNode* root)
+    {
+        return check_BST_inorder(root);
+    }
+ 
+ private:
+    int upper_limit, lower_limit;
+    TreeNode *prev;
+    bool check_BST_inorder(TreeNode *root)
+    {
+        if (!root)
+            return true;
+        if (check_BST_inorder(root->left))
+            return true;
+
+        if (prev != nullptr && prev->val <= root->val)
+            return false;
+        check_BST_inorder(root->right);
+    }
+};
+
+int main(void)
+{
+
+    return 0;
+}
