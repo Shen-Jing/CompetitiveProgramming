1e74e2adac54e190f5a7ae8fb421fb71e09742d0
ShenJing <shenjingnerve@gmail.com>
2021-04-27 21:48:36 +0800

    [FEAT] LC 104: Rewriting with C++ (a warm up before 543. diameter)

---
+++ LeetCode/LeetCode101-200/LeetCode104_Maximum Depth of Binary Tree.cpp
@@ -0,0 +1,57 @@
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <fstream>
+#include <functional>
+#include <initializer_list>
+#include <iostream>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <tuple>
+#include <type_traits>
+#include <utility>
+#include <regex>
+#include <set>
+#include <span>
+#include <stack>
+#include <sstream>
+#include <string>
+#include <type_traits>
+#include <vector>
+#include "utils.hpp"
+
+using namespace std;
+
+static auto io = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    return nullptr;
+}();
+
+class Solution
+{
+ public:
+    int maxDepth(TreeNode* root)
+    {
+        return get_max_depth(root);
+    }
+ 
+ private:
+    int get_max_depth(TreeNode *root)
+    {
+        if (!root)
+            return 0;
+        return 1 + max(maxDepth(root->left), maxDepth(root->right));
+    }
+};
+
+int main(void)
+{
+
+    return 0;
+}
