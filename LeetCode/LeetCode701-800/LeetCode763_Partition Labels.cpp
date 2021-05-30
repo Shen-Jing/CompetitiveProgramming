9d4fc7a1b5ee7b6bdd8a8554f0845940a5819ad3
ShenJing <shenjingnerve@gmail.com>
2021-05-30 17:26:02 +0800

    [FEAT] LC 763: Incorrect draft

---
+++ LeetCode/LeetCode701-800/LeetCode763_Partition Labels.cpp
@@ -0,0 +1,65 @@
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <fstream>
+#include <functional>
+#include <initializer_list>
+#include <iostream>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <tuple>
+#include <type_traits>
+#include <utility>
+#include <regex>
+#include <set>
+#include <span>
+#include <stack>
+#include <sstream>
+#include <string>
+#include <type_traits>
+#include <vector>
+
+using namespace std;
+
+static auto io = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    return nullptr;
+}();
+
+class Solution
+{
+ public:
+    vector<int> partitionLabels(string s)
+    {
+        const auto len = s.length();
+        array<size_t, 26> last_pos;
+
+        for (size_t i = 0; i < len; ++i)
+            last_pos[s[i] - 'a'] = i;
+
+        vector<int> partition_lens;
+        size_t last_partition_pos = 0;
+        for (size_t i = 0; i < len; ++i)
+        {
+            if (i == last_pos[s[i] - 'a'])
+            {
+                partition_lens.emplace_back(i - last_partition_pos + 1);
+                last_partition_pos = i;
+            }
+        }
+        partition_lens.emplace_back(len - last_partition_pos);
+        return partition_lens;
+    }
+};
+
+int main(void)
+{
+
+    return 0;
+}
