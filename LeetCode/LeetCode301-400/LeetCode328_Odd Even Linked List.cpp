2671c71942fb1a2d3f0881b55a1fd3dd7347b466
ShenJing <shenjingnerve@gmail.com>
2021-12-06 20:58:49 +0800

    [FEAT] LC 328 (AC): Iterate every node and split them into 2 lists
    【Daily LeetCoding Challenge】12/2

---
+++ LeetCode/LeetCode301-400/LeetCode328_Odd Even Linked List.cpp
@@ -0,0 +1,62 @@
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <fstream>
+#include <functional>
+#include <initializer_list>
+#include <iostream>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <tuple>
+#include <type_traits>
+#include <utility>
+#include <regex>
+#include <set>
+#include <span>
+#include <stack>
+#include <sstream>
+#include <string>
+#include <type_traits>
+#include <vector>
+#include "utils.hpp"
+
+using namespace std;
+
+static auto io = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    return nullptr;
+}();
+
+class Solution
+{
+ public:
+    ListNode* oddEvenList(ListNode *head)
+    {
+        if (!head)
+            return head;
+        ListNode *odd_head(head), *even_head, *odd, *even;
+        odd = odd_head;
+        even = even_head = odd_head->next;
+        while (even && even->next)
+        {
+            odd->next = even->next;
+            odd = odd->next;
+            even->next = odd->next;
+            even = even->next;
+        }
+        odd->next = even_head;
+        return odd_head;
+    }
+};
+
+int main(void)
+{
+
+    return 0;
+}
