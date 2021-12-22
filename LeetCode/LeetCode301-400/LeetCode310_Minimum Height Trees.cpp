a5f7609e71e882526a559d8209cec755ef66acdf
ShenJing <shenjingnerve@gmail.com>
2021-12-22 10:42:23 +0800

    [REFACTOR] LC 310 (TLE): Add comment for brute force & refactor to 1 pass
    67 / 70 test cases passed. (fail: `n`: 10002)

--- LeetCode/LeetCode301-400/LeetCode310_Minimum Height Trees.cpp
+++ LeetCode/LeetCode301-400/LeetCode310_Minimum Height Trees.cpp
@@ -31,46 +31,54 @@ class Solution
  public:
     vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges)
     {
-        vector<vector<int>> adj_list(n, vector<int>());
+        adj_list_.assign(n, vector<int>());
         /* Preprocessing */
         for (const auto &edge : edges)
         {
-            adj_list[edge[0]].emplace_back(edge[1]);
-            adj_list[edge[1]].emplace_back(edge[0]);
+            adj_list_[edge[0]].emplace_back(edge[1]);
+            adj_list_[edge[1]].emplace_back(edge[0]);
         }
-        return my_DFS(n, adj_list);
+        return brute_force_by_DFS(n, adj_list_);
     }
     
  private: 
     int min_height_{numeric_limits<int>::max()};
+    vector<vector<int>> adj_list_;
 
-    vector<int> my_DFS(int n, vector<vector<int>>& adj_list)
+    vector<int> brute_force_by_DFS(int n, vector<vector<int>>& adj_list)
     {
-        vector<int> heights, MHT_roots;
+        vector<int> MHT_roots;
+        /* Brute force:
+            Consider each one of the nodes as root
+            and calculating the height of the tree */
+        min_height_ = n;
         for (int node_i = 0; node_i < n; ++node_i)
         {
             vector<bool> visited(n, false);
-            int height{0};
-            heights.emplace_back(DFS(adj_list, visited, node_i, 0, height));
-            min_height_ = min(min_height_, height);
-        }
-        for (int node_i = 0; node_i < n; ++node_i)
-            if (heights[node_i] == min_height_)
+            int height = DFS(adj_list, visited, node_i);
+            if (height < min_height_)
+            {
+                /* Get ans by 1 pass */
+                MHT_roots.clear();
+                min_height_ = height;
+            }
+            if (height == min_height_)
                 MHT_roots.emplace_back(node_i);
+        }
         return MHT_roots;
     }
 
     int DFS(const vector<vector<int>> &edges, vector<bool> &visited,
-            const int &node, int path_length, int &height)
+            const int &node)
     {
-        if (visited[node])
-            return 0;
+        int height{0};
         visited[node] = true;
         for (const auto &neighbor : edges[node])
         {
-            DFS(edges, visited, neighbor, path_length + 1, height);
+            if (!visited[neighbor])
+                height = max(height,
+                             1 + DFS(edges, visited, neighbor));
         }
-        height = max(height, path_length);
         return height;
     }
 };
