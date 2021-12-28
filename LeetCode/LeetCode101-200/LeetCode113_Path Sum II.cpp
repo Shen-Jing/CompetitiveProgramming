06621cd088795641686aca69b718bc3676dea4a9
ShenJing <shenjingnerve@gmail.com>
2021-12-28 14:18:14 +0800

    [FEAT] LC 113 (AC): Modify into backtracking (improve on time & memory).

--- LeetCode/LeetCode101-200/LeetCode113_Path Sum II.cpp
+++ LeetCode/LeetCode101-200/LeetCode113_Path Sum II.cpp
@@ -41,28 +41,30 @@ class Solution
     vector<vector<int>> pathSum(TreeNode *root, int targetSum)
     {
         vector<int> path_list;
-        DFS(root, targetSum, path_list);
+        DFS_backtrack(root, targetSum, path_list);
         return ans_lists_;
     }
 
  private:
     vector<vector<int>> ans_lists_;
     /* Preorder DFS */
-    void DFS(TreeNode *root, int target_sum, vector<int> path_list)
+    void DFS_backtrack(TreeNode *root, int target_sum, vector<int> &path_list)
     {
         if (!root)
             return;
         
         path_list.emplace_back(root->val);
-        if (!root->left && !root->right)
+        if (!root->left && !root->right && root->val == target_sum)
         {
-            if (root->val == target_sum)
-                ans_lists_.emplace_back(path_list);
-            return;
+            ans_lists_.emplace_back(path_list);
         }
-        target_sum -= root->val;
-        DFS(root->left, target_sum, path_list);
-        DFS(root->right, target_sum, path_list);
+        else
+        {
+            target_sum -= root->val;
+            DFS_backtrack(root->left, target_sum, path_list);
+            DFS_backtrack(root->right, target_sum, path_list);
+        }
+        path_list.pop_back();
     }
 };
 
