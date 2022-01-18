b229c01013f40f08e13e0321c35ae26c98c33341
ShenJing <shenjingnerve@gmail.com>
2022-01-18 11:33:12 +0800

    [FEAT] LC 31 (AC): (2021-08-08) STL `next_permutation`

---
+++ LeetCode/LeetCode1-100/LeetCode31_Next Permutation.cpp
@@ -0,0 +1,55 @@
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <fstream>
+#include <functional>
+#include <initializer_list>
+#include <iostream>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <tuple>
+#include <type_traits>
+#include <utility>
+#include <regex>
+#include <set>
+#include <span>
+#include <stack>
+#include <sstream>
+#include <string>
+#include <type_traits>
+#include <vector>
+
+using namespace std;
+
+static auto io = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    return nullptr;
+}();
+
+class Solution
+{
+ public:
+    void nextPermutation(vector<int> &nums)
+    {
+        return;
+    }
+
+ private:
+    void STL_next_permutation(vector<int> &nums)
+    {
+        next_permutation(nums.begin(), nums.end());
+        return;
+    }
+};
+
+int main(void)
+{
+
+    return 0;
+}
