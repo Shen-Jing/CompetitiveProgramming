918c42bc7c56857c9ae269f6d78f99cf3a9a2bf3
ShenJing <shenjingnerve@gmail.com>
2022-01-18 13:52:46 +0800

    [FEAT] LC 31 (AC): Linear, one pass to get next permutation

--- LeetCode/LeetCode1-100/LeetCode31_Next Permutation.cpp
+++ LeetCode/LeetCode1-100/LeetCode31_Next Permutation.cpp
@@ -37,6 +37,7 @@ class Solution
  public:
     void nextPermutation(vector<int> &nums)
     {
+        linear_solve(nums);
         return;
     }
 
@@ -45,6 +46,27 @@ class Solution
     {
         next_permutation(nums.begin(), nums.end());
         return;
+    }
+
+    void linear_solve(vector<int> &nums)
+    {
+        int idx = (nums.size() - 1) - 1;
+        for ( ; idx >= 0; --idx)
+            if (nums[idx] < nums[idx + 1])
+                break;
+        if (idx < 0)
+        {
+            sort(nums.begin(), nums.end());
+            return;
+        }
+
+        for (int j = nums.size() - 1; j >= 0; --j)
+            if (nums[j] > nums[idx])
+            {
+                swap(nums[j], nums[idx]);
+                break;
+            }
+        reverse(nums.begin() + idx + 1, nums.end());
     }
 };
 
