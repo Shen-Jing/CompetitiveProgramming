49275a2c34b66cc8e6107f83b11ece91af9b0972
ShenJing <shenjingnerve@gmail.com>
2022-01-18 10:21:00 +0800

    [REFACTOR] LC 46: Add necessary header.

--- LeetCode/LeetCode1-100/LeetCode46_Permutations.cpp
+++ LeetCode/LeetCode1-100/LeetCode46_Permutations.cpp
@@ -1,3 +1,37 @@
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <fstream>
+#include <functional>
+#include <initializer_list>
+#include <iostream>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <tuple>
+#include <type_traits>
+#include <utility>
+#include <regex>
+#include <set>
+#include <span>
+#include <stack>
+#include <sstream>
+#include <string>
+#include <type_traits>
+#include <vector>
+
+using namespace std;
+
+static auto io = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    return nullptr;
+}();
+
 class Solution {
 public:
     vector<vector<int>> permute(vector<int>& nums) {
