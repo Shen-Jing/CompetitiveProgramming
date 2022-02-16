9ab6325d0f77962668381526753a26f4c1ca496f
ShenJing <shenjingnerve@gmail.com>
2022-02-16 09:45:38 +0800

    [REFACTOR] LC 189: By `std::rotate`
    Reduce size of k by modulo.

--- LeetCode/LeetCode101-200/LeetCode189_Rotate Array.cpp
+++ LeetCode/LeetCode101-200/LeetCode189_Rotate Array.cpp
@@ -1,8 +1,52 @@
-class Solution {
-public:
-    void rotate(vector<int>& nums, int k) {
-        int sz{nums.size()};
-        int steps = (k > sz) ? k - sz : k;
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <fstream>
+#include <functional>
+#include <initializer_list>
+#include <iostream>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <tuple>
+#include <type_traits>
+#include <utility>
+#include <regex>
+#include <set>
+#include <span>
+#include <stack>
+#include <sstream>
+#include <string>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+static auto io = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    return nullptr;
+}();
+
+class Solution
+{
+ public:
+    void rotate(vector<int>& nums, int k)
+    {
+        return rotate_STL(nums, k);
+    }
+
+ private:
+    void rotate_STL(vector<int> &nums, int k)
+    {
+        int sz = nums.size();
+        int steps = (k > sz) ? k % sz : k;
         std::rotate(nums.rbegin(), nums.rbegin() + steps, nums.rend());
         return;
     }
