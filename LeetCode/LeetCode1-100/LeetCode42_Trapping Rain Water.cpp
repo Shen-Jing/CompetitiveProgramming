4e97d3951253e1de98da994d7f96184ef2af8531
ShenJing <shenjingnerve@gmail.com>
2022-02-17 11:30:30 +0800

    [FEAT] LC 42 (AC): DP (already read the Solution in 1/26)

--- LeetCode/LeetCode1-100/LeetCode42_Trapping Rain Water.cpp
+++ LeetCode/LeetCode1-100/LeetCode42_Trapping Rain Water.cpp
@@ -37,7 +37,7 @@ class Solution
  public:
     int trap(vector<int> &height)
     {
-        return brute_force(height);
+        return DP(height);
     }
 
  private:
@@ -60,13 +60,36 @@ class Solution
         }
         return trap_water;
     }
+
+    int DP(vector<int> &heights)
+    {
+        int sz = heights.size();
+        vector<int> all_left_max(sz, 0), all_right_max(sz, 0);
+        all_left_max[0] = heights[0];
+        all_right_max[sz - 1] = heights[sz - 1];
+        for (int i = 1; i < sz; ++i)
+        {
+            all_left_max[i] = max(all_left_max[i - 1], heights[i]);
+            all_right_max[(sz - 1) - i] = max(all_right_max[(sz - 1) - i + 1], heights[(sz - 1) - i]);
+        }
+
+        int trap_water{0};
+        for (int i = 1; i < sz - 1; ++i)
+            trap_water += min(all_left_max[i], all_right_max[i]) - heights[i];
+        return trap_water;
+    }
 };
 
 int main(void)
 {
     Solution sol;
 
-    vector<int> height = {4,2,0,3,2,5};
+    vector<int> height;
+
+    height = {0,1,0,2,1,0,1,3,2,1,2,1};
+    sol.trap(height);
+
+    height = {4,2,0,3,2,5};
     sol.trap(height);
 
     return 0;
