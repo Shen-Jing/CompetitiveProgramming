f7ee3181ff1a59101de4f5cfaaa2a389612b6d9f
ShenJing <shenjingnerve@gmail.com>
2022-03-26 22:31:58 +0800

    [FEAT] LC 88 (AC): Inplace (idea from practice with Sean)

--- LeetCode/LeetCode1-100/LeetCode88_Merge Sorted Array.cpp
+++ LeetCode/LeetCode1-100/LeetCode88_Merge Sorted Array.cpp
@@ -39,6 +39,8 @@ class Solution
  public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n)
     {
+        // not_inplace(nums1, m, nums2, n);
+        merge_inplace(nums1, m, nums2, n);
     }
 
  private:
@@ -66,6 +68,38 @@ class Solution
         for (; idx2 < n; ++idx2)
             ans.emplace_back(nums2[idx2]);
         swap(nums1, ans);
+    }
+
+    /* sorted 
+    A = [3, 5, 7] 
+    B = [1, 2, 4, 6]
+    Ans = [1, 2, 3, 4, 5, 6, 7] 
+    */
+    void merge_inplace(vector<int>& nums1, int m, vector<int>& nums2, int n)
+    {
+        int idx1{0}, idx2{0};
+        for (; idx1 < m && idx2 < n; ++idx1)
+        {
+            if (nums1[idx1] > nums2[idx2])
+            {
+                swap(nums1[idx1], nums2[idx2]);
+                int tmp = idx2;
+                for (int j = idx2 + 1; j < n; ++j)
+                {
+                    if (nums2[j] < nums2[idx2])
+                    {
+                        swap(nums2[j], nums2[idx2]);
+                        idx2 = j;
+                    }
+                    else
+                        break;
+                }
+                idx2 = tmp;
+            }
+        }
+        for (int i = 0; i < n; ++i)
+            nums1[m + i] = nums2[i];
+        return;
     }
 };
 
