2c27ccc04f4385313176eea76680b14868966fa2
ShenJing <shenjingnerve@gmail.com>
2022-04-19 23:33:05 +0800

    [FEAT] LC 147 (AC): Same with the Solution
    【Daily LeetCoding Challenge】12/15

--- LeetCode/LeetCode101-200/LeetCode147_Insertion Sort List.cpp
+++ LeetCode/LeetCode101-200/LeetCode147_Insertion Sort List.cpp
@@ -38,32 +38,25 @@ class Solution
  public:
     ListNode* insertionSortList(ListNode *head)
     {
-        ListNode *dummy_min = new ListNode(numeric_limits<int>::min()),
-                 *dummy_max = new ListNode(numeric_limits<int>::max()),
-                 *dummy = new ListNode();
-        ListNode *sorted_head{dummy_min}, *prev{dummy}, *curr{head};
-        dummy_min->next = dummy_max;
-        dummy->next = head;
+        ListNode dummy(0, nullptr);
+        ListNode *prev = &dummy, *curr{head};
 
+        /* Consider curr as an unsorted index/iterator */
         while (curr)
         {
-            ListNode *sorted_prev{sorted_head}, *sorted_iter{sorted_head};
-            while (sorted_iter)
+            ListNode *prev{&dummy};
+            while (prev->next && prev->next->val < curr->val)
             {
-                if (sorted_iter->val <= curr->val && curr->val <= sorted_iter->next->val)
-                    break;
-                sorted_iter = sorted_iter->next;
+                prev = prev->next;
             }
-            /* Unsorted node is removed from original (unsorted) list */
-            prev->next = curr->next;
             /* Inserting unsorted node into sorted list */
-            curr->next = sorted_iter->next;
-            sorted_iter->next = curr;
+            ListNode *temp_next = curr->next;
+            curr->next = prev->next;
+            prev->next = curr;
             /* Move forward (iterator) */
-            curr = prev->next;
+            curr = temp_next;
         }
-        delete dummy, dummy_min, dummy_max;
-        return sorted_head->next;
+        return dummy.next;
     }
 };
 
