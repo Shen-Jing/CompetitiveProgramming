6631f95438f88997873bab4492a94fb6bec40a9b
ShenJing <shenjingnerve@gmail.com>
2022-04-23 17:47:30 +0800

    [FEAT] LC 700 (AC): Recursion (first try)

---
+++ LeetCode/LeetCode601-700/LeetCode700_Search in a Binary Search Tree.cpp
@@ -0,0 +1,57 @@
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <fstream>
+#include <functional>
+#include <initializer_list>
+#include <iostream>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <tuple>
+#include <type_traits>
+#include <utility>
+#include <regex>
+#include <set>
+#include <span>
+#include <stack>
+#include <sstream>
+#include <string>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#include "utils.hpp"
+
+using namespace std;
+
+static auto io = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    return nullptr;
+}();
+
+class Solution
+{
+ public:
+    TreeNode* searchBST(TreeNode* root, int val)
+    {
+        if (!root)
+            return nullptr;
+        if (root->val == val)
+            return root;
+        if (val < root->val)
+            return searchBST(root->left, val);
+        return searchBST(root->right, val);
+    }
+};
+
+int main(void)
+{
+
+    return 0;
+}
