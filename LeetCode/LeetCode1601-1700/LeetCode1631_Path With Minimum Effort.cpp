cbd5005d0df9af237babcea1c21b28e2559a76b6
ShenJing <shenjingnerve@gmail.com>
2022-05-02 11:55:11 +0800

    [FEAT] LC 1631 (AC): Union & Find (learned from the Solution)
    【Daily LeetCoding Challenge】04/28

--- LeetCode/LeetCode1601-1700/LeetCode1631_Path With Minimum Effort.cpp
+++ LeetCode/LeetCode1601-1700/LeetCode1631_Path With Minimum Effort.cpp
@@ -34,6 +34,63 @@ static auto io = [](){
     return nullptr;
 }();
 
+struct Edge
+{
+    int u, v;
+    int diff;
+    Edge(int u = 0, int v = 0, int d = 0) : u(u), v(v), diff(d) {}
+};
+class UnionFind
+{
+ public:
+    vector<int> parents;
+    vector<int> ranks;
+    vector<Edge> edges;
+    UnionFind(const vector<vector<int>> &heights)
+    {
+        int rows = heights.size(), cols = heights[0].size();
+        parents.assign(rows * cols, 0);
+        ranks.assign(rows * cols, 0);
+
+        for (int r = 0; r < rows; ++r)
+            for (int c = 0; c < cols; ++c)
+            {
+                if (r > 0)
+                {
+                    edges.emplace_back(r * cols + c, (r - 1) * cols + c, abs(heights[r][c] - heights[r - 1][c]));
+                }
+                if (c > 0)
+                {
+                    edges.emplace_back(r * cols + c, r * cols + (c - 1), abs(heights[r][c] - heights[r][c - 1]));
+                }
+                parents[(r * cols + c)] = r * cols + c;
+            }
+    }
+
+    int find_root(int x)
+    {
+        return (parents[x] == x) ? x : parents[x] = find_root(parents[x]);
+    }
+
+    void union_by_rank(int x, int y)
+    {
+        int root_x = find_root(x),
+            root_y = find_root(y);
+        if (root_x != root_y)
+        {
+            if (ranks[root_x] > ranks[root_y])
+                parents[root_y] = root_x;
+            else if (ranks[root_y] > ranks[root_x])
+                parents[root_x] = root_y;
+            else
+            {
+                parents[root_y] = root_x;
+                ++ranks[root_x];
+            }
+        }
+    }
+};
+
 class Solution
 {
  public:
@@ -41,10 +98,11 @@ class Solution
     {
         rows_ = heights.size();
         cols_ = heights[0].size();
-        visited_.assign(rows_, vector<bool>(cols_, false));
+        return union_and_find(heights);
+        // visited_.assign(rows_, vector<bool>(cols_, false));
         // min_efforts_.assign(rows_, vector<int>(cols_, numeric_limits<int>::max()));
-        int effort{0};
-        brute_force_backtrack(heights, 0, 0, effort);
+        // int effort{0};
+        // brute_force_backtrack(heights, 0, 0, effort);
         return ans_;
     }
 
@@ -83,10 +141,39 @@ class Solution
         }
         visited_[cur_r][cur_c] = false;
     }
+
+    int union_and_find(const vector<vector<int>> &heights)
+    {
+        if (rows_ == 1 && cols_ == 1)
+            return 0;
+        UnionFind union_find(heights);
+        sort(union_find.edges.begin(), union_find.edges.end(), [](const auto &lhs, const auto &rhs)
+            {
+                return lhs.diff < rhs.diff;
+            });
+        
+        for (const auto &e : union_find.edges)
+        {
+            int src = e.u, 
+                dst = e.v;
+            union_find.union_by_rank(src, dst);
+            if (union_find.find_root(0) == union_find.find_root(rows_ * cols_ - 1))
+                return e.diff;
+        }
+        return -1;
+    }
 };
 
 int main(void)
 {
+    Solution sol;
+    vector<vector<int>> heights;
+
+    heights = {{3}};
+    cout << sol.minimumEffortPath(heights) << "\n";
+
+    heights = {{4,3,4,10,5,5,9,2},{10,8,2,10,9,7,5,6},{5,8,10,10,10,7,4,2},{5,1,3,1,1,3,1,9},{6,4,10,6,10,9,4,6}};
+    cout << sol.minimumEffortPath(heights) << "\n";
 
     return 0;
 }
