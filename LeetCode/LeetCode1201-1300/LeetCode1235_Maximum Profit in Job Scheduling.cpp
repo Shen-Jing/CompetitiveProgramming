08e9211913dc1608708e80f95be1d2f2e8ce3e76
ShenJing <shenjingnerve@gmail.com>
2022-05-05 09:39:19 +0800

    [FEAT] LC 1235 (AC): Top Down DP

--- LeetCode/LeetCode1201-1300/LeetCode1235_Maximum Profit in Job Scheduling.cpp
+++ LeetCode/LeetCode1201-1300/LeetCode1235_Maximum Profit in Job Scheduling.cpp
@@ -38,10 +38,30 @@ class Solution
  public:
     int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit)
     {
+        return top_down(startTime, endTime, profit);
     }
 
  private:
-    int my_dp(vector<int>& startTime, vector<int>& endTime, vector<int>& profit)
+    vector<int> max_profits;
+    int find_max_profit_top_down(vector<vector<int>> &jobs, int i)
+    {
+        if (i == jobs.size())
+            return 0;
+        if (max_profits[i] != -1)
+            return max_profits[i];
+        
+        int nxt_idx = lower_bound(jobs.begin(), jobs.end(), jobs[i][1],
+            [](const auto &ele, int end_time)
+            {
+                return ele[0] < end_time;
+            }) - jobs.begin();
+        /* Skip the current job or not */
+        max_profits[i] = max(find_max_profit_top_down(jobs, i + 1), 
+                             find_max_profit_top_down(jobs, nxt_idx) + jobs[i][2]);
+        return max_profits[i];
+    }
+
+    int top_down(vector<int>& startTime, vector<int>& endTime, vector<int>& profit)
     {
         vector<vector<int>> jobs;
         int sz = profit.size();
@@ -51,25 +71,20 @@ class Solution
         }
         sort(jobs.begin(), jobs.end());
 
-        vector<int> max_profits(sz, 0);
-        int max_profit = 0;
-        for (int i = 0; i < sz; ++i)
-        {
-            /* The current max profit */
-            max_profits[i] = jobs[i][2];
-            for (int j = 0; j < i; ++j)
-            {
-                if (jobs[i][0] >= jobs[j][1])
-                    max_profits[i] = max(max_profits[i], jobs[i][2] + max_profits[j]);
-            }
-            max_profit = max(max_profit, max_profits[i]);
-        }
-        return max_profit;
+        max_profits.assign(sz, -1);
+        return find_max_profit_top_down(jobs, 0);
     }
 };
 
 int main(void)
 {
+    Solution sol;
+    vector<int> startTime, endTime, profit;
+
+    startTime = {1,2,3,3};
+    endTime = {3,4,5,6};
+    profit = {50,10,40,70};
+    sol.jobScheduling(startTime, endTime, profit);
 
     return 0;
 }
