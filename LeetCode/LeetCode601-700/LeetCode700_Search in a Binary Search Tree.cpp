e8c8d10e9b5e0737deba06b97a5f4d1f7e8b6cbb
ShenJing <shenjingnerve@gmail.com>
2022-05-12 14:57:46 +0800

    [FEAT] LC 700 (AC): More tidy recursive version

--- LeetCode/LeetCode601-700/LeetCode700_Search in a Binary Search Tree.cpp
+++ LeetCode/LeetCode601-700/LeetCode700_Search in a Binary Search Tree.cpp
@@ -40,13 +40,15 @@ class Solution
  public:
     TreeNode* searchBST(TreeNode* root, int val)
     {
-        if (!root)
-            return nullptr;
-        if (root->val == val)
+
+    }
+
+ private:
+    TreeNode* search_BST_recursive(TreeNode *root, const int val)
+    {
+        if (!root || root->val == val)
             return root;
-        if (val < root->val)
-            return searchBST(root->left, val);
-        return searchBST(root->right, val);
+        return (val < root->val) ? searchBST(root->left, val) : searchBST(root->right, val);
     }
 };
 
