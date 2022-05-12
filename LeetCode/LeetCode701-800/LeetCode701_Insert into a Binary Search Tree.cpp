ceb3f89b0933f9ed60b468f0cd3afee1bd2cd139
ShenJing <shenjingnerve@gmail.com>
2022-05-12 15:27:06 +0800

    [FEAT] LC 701 (AC): Recursion
    【Daily LeetCoding Challenge】1/12

---
+++ LeetCode/LeetCode701-800/LeetCode701_Insert into a Binary Search Tree.cpp
@@ -0,0 +1,62 @@
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <fstream>
+#include <functional>
+#include <initializer_list>
+#include <iostream>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <regex>
+#include <set>
+#include <span>
+#include <stack>
+#include <sstream>
+#include <string>
+#include <tuple>
+#include <type_traits>
+#include <utility>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#include "utils.hpp"
+
+using namespace std;
+
+static auto io = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    return nullptr;
+}();
+
+class Solution
+{
+ public:
+    TreeNode* insertIntoBST(TreeNode *root, int val)
+    {
+    }
+
+ private:
+    TreeNode* insert_recursive(TreeNode *root, int val)
+    {
+        if (!root)
+            return new TreeNode(val, nullptr, nullptr);
+
+        if (val > root->val)
+            root->right = insertIntoBST(root->right, val);
+        else
+            root->left = insertIntoBST(root->left, val);
+        return root;
+    }
+};
+
+int main(void)
+{
+
+    return 0;
+}
