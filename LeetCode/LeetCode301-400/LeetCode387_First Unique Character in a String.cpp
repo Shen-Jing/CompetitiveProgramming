d0a3a46795eed59af3dcfd1791b275631d78eb44
ShenJing <shenjingnerve@gmail.com>
2022-08-23 21:35:46 +0800

    [FEAT] LC 387 (AC): Rewritten for 8/16's challenge

--- LeetCode/LeetCode301-400/LeetCode387_First Unique Character in a String.cpp
+++ LeetCode/LeetCode301-400/LeetCode387_First Unique Character in a String.cpp
@@ -1,11 +1,49 @@
-//static auto io = [](){
-//    std::ios::sync_with_stdio(false);
-//    std::cin.tie(nullptr);
-//    return nullptr;
-//}();
-class Solution {
-public:
-    int firstUniqChar(string s) {
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <fstream>
+#include <functional>
+#include <initializer_list>
+#include <iostream>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <regex>
+#include <set>
+#include <span>
+#include <stack>
+#include <sstream>
+#include <string>
+#include <tuple>
+#include <type_traits>
+#include <utility>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+static auto io = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    return nullptr;
+}();
+
+class Solution
+{
+ public:
+    int firstUniqChar(string s)
+    {
+    }
+
+ private:
+    /* Written on 2019 */
+    int hashtable_and_find_forward(string s)
+    {
         std::map<char, int> wrd_rcd;
         for (int i = 0; i < s.size(); ++i)
         {
@@ -19,4 +57,24 @@ public:
         }
         return -1;
     }
+
+    int two_pass_by_hash_table(string s)
+    {
+        unordered_map<int, int> char_cnts;
+        for (const auto &ch : s)
+            ++char_cnts[ch];
+        
+        for (int i = 0; i < s.length(); ++i)
+        {
+            if (char_cnts[s[i]] == 1)
+                return i;
+        }
+        return -1;
+    }
 };
+
+int main(void)
+{
+
+    return 0;
+}
