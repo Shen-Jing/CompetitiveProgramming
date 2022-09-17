d201859152e2880a350da469c60d232f9a9ba892
ShenJing <shenjingnerve@gmail.com>
2022-09-17 20:48:23 +0800

    [FEAT] LC 2095 (AC): hash table & sorting
    From my intuition. 🤗
    【Daily LeetCoding Challenge】09/15

---
+++ LeetCode/LeetCode2001-2100/LeetCode2095_Delete the Middle Node of a Linked List.cpp
@@ -0,0 +1,51 @@
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <fstream>
+#include <functional>
+#include <initializer_list>
+#include <iostream>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <tuple>
+#include <type_traits>
+#include <utility>
+#include <regex>
+#include <set>
+#include <span>
+#include <stack>
+#include <sstream>
+#include <string>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#include "utils.hpp"
+
+using namespace std;
+
+static auto io = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    return nullptr;
+}();
+
+class Solution
+{
+ public:
+    ListNode* deleteMiddle(ListNode* head)
+    {
+        
+    }
+};
+
+int main(void)
+{
+
+    return 0;
+}
