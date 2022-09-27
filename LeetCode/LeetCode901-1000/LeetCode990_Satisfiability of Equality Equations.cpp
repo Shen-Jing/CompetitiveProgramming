6480df4a33603cc8e72e156878544d730671ac55
ShenJing <shenjingnerve@gmail.com>
2022-09-27 15:50:31 +0800

    [FEAT] LC 990: Union & Find draft, struggle to figure out different situations

---
+++ LeetCode/LeetCode901-1000/LeetCode990_Satisfiability of Equality Equations.cpp
@@ -0,0 +1,125 @@
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <fstream>
+#include <functional>
+#include <initializer_list>
+#include <iostream>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <regex>
+#include <set>
+#include <span>
+#include <stack>
+#include <sstream>
+#include <string>
+#include <tuple>
+#include <type_traits>
+#include <utility>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+static auto io = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    return nullptr;
+}();
+
+struct UnionFind
+{
+ public:
+    UnionFind()
+    {
+        fill(root.begin(), root.end(), -1);  // -1: unvisited
+    }
+
+    int get_root(int n)
+    {
+        return root[n];
+    }
+
+    void set_root(int n, int r)
+    {
+        root[n] = r;
+    }
+
+    int find_root(int n)
+    {
+        return root[n] = (root[n] == n) ? n : (find_root(root[n]));
+    }
+
+    void union_groups(int x, int y)
+    {
+        if (find_root(x) == find_root(y))
+            return;
+        int r_x = root[x],
+            r_y = root[y];
+        root[y] = r_x;
+    }
+
+ private:
+    static const int alphabet_size{26};
+    array<int, UnionFind::alphabet_size> root;
+};
+
+class Solution
+{
+ public:
+    bool equationsPossible(vector<string> &equations)
+    {
+        return by_union_find(equations);
+    }
+
+ private:
+    bool by_union_find(vector<string> &equations)
+    {
+        UnionFind uf;
+        for (const auto &eq : equations)
+        {
+            int x = eq.front(),
+                y = eq.back();
+            const char &sign = eq[1];
+            if (x > y)
+                swap(x, y);
+            
+            /**
+             * [Key points] 
+             * - If the inequality comes first, how to handle the equality afterwards?
+             * - If one of operand is unvisited?
+             */
+            if (sign == '=')
+            {
+                if (uf.get_root(x) != uf.get_root(y))
+                    return false;
+                if (uf.get_root(x) == -1)
+                {
+                    uf.set_root(x, x);
+                    uf.set_root(y, y);
+                }
+                uf.union_groups(x, y);
+            }
+            if (uf.get_root(x) == -1)
+                uf.set_root(x, x);
+            if (uf.get_root(y) == -1)
+                uf.set_root(y, y);
+
+            if (sign == '!' && uf.get_root(x) == uf.get_root(y))
+                return false;
+        }
+        return true;
+    }
+};
+
+int main(void)
+{
+
+    return 0;
+}
