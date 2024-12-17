2bfcb8e8fb01e1c3aef91422ca9f3e67887c80bc
ShenJing <shenjingnerve@gmail.com>
2024-12-17 12:43:35 +0800

    [FEAT] LC 284 (AC)

--- LeetCode/LeetCode201-300/LeetCode284_Peeking Iterator.cpp
+++ LeetCode/LeetCode201-300/LeetCode284_Peeking Iterator.cpp
@@ -17,6 +17,7 @@
 #include <span>
 #include <stack>
 #include <sstream>
+#include <stdexcept>
 #include <string>
 #include <tuple>
 #include <type_traits>
@@ -54,27 +55,53 @@ class Iterator {
 
 class PeekingIterator : public Iterator {
 public:
-  PeekingIterator(const vector<int>& nums) : Iterator(nums) {
+  PeekingIterator(const vector<int>& nums) : Iterator(nums)
+  {
       // Initialize any member here.
       // **DO NOT** save a copy of nums and manipulate it directly.
       // You should only use the Iterator interface methods.
-      
+      has_next = Iterator::hasNext();
+      if (has_next)
+      {
+          next_value = Iterator::next();
+      }
   }
   
-     // Returns the next element in the iteration without advancing the iterator.
-  int peek() {
-         
+  // Returns the next element in the iteration without advancing the iterator.
+  int peek()
+  {
+      return next_value;
   }
   
   // hasNext() and next() should behave the same as in the Iterator interface.
   // Override them if needed.
-  int next() {
-      
+  int next()
+  {
+      if (!has_next)
+      {
+          throw std::invalid_argument("test");
+          return -1;
+      }
+      else
+      {
+          int result = next_value;
+          has_next = Iterator::hasNext();
+          if (has_next)
+          {
+              next_value = Iterator::next();
+          }
+          return result;
+      }
   }
   
-  bool hasNext() const {
-      
+  bool hasNext() const
+  {
+      return has_next;
   }
+
+private:
+  bool has_next = false;
+  int next_value;
 };
 
 int main(void)
